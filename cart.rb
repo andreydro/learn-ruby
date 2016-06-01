9de1e74e1e3280b1c80029da871c45e9cf523411
dro14 <and-rey-2309@mail.ru>
2016-06-01 23:08:21 +0300

    Finish 31 episode

--- cart.rb
+++ cart.rb
@@ -3,6 +3,7 @@ class Cart
   attr_reader :items
 
   include ItemContainer
+  class ItemNotSupported < StandardError; end
 
   def initialize(owner)
     @items =Array.new
@@ -11,20 +12,20 @@ class Cart
 
   def save_to_file
     File.open("#{@owner}_cart.txt", "w") do |f|
-      @items.each { |i| f.puts i } # car:100:50
+      @items.each do |i|
+          raise ItemNotSupported if i.class == VirtualItem
+          f.puts i
+      end
     end
   end
 
   def read_from_file
-    begin
-      10/0
       #return unless File.exists?("#{@owner}_cart.txt")
       File.readlines("#{@owner}_cart.txt").each { |i| @items << i.to_real_item}
       @items.uniq!
     rescue Errno::ENOENT
       File.open("#{@owner}_cart.txt", "w") {}
       puts "file #{@owner}_cart.txt created"
-    end
   end
 
 end
