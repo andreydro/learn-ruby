5fe69bb57db23530c04ba769ce8756635c3428c1
dro14 <and-rey-2309@mail.ru>
2016-06-05 14:30:07 +0300

    Finish 41 episode

--- init.rb
+++ init.rb
@@ -12,31 +12,11 @@ StoreApplication.config do |app|
 
 end
 
-unless StoreApplication.frozen?
-  StoreApplication.name = "My name"
-end
-#StoreApplication::Admin.email = "new@email.com"
-puts StoreApplication::Admin.email
-
-
-p StoreApplication.environment
-p StoreApplication.name
-p StoreApplication::Admin.email
-p StoreApplication::Admin.login
-
-
 @items = []
 @items << AntiqueItem.new("car", price: 101, weight: 100)
 @items << RealItem.new({ :weight => 100, :price => 101, :name => "kettle"})
 @items << RealItem.new({:weight => 100, :price => 101, :name => "dishwasher"})
 
-@items.each {|i| puts i.name }
-
-cart = Cart.new("roman")
-cart.add_item RealItem.new({ :weight => 100, :price => 101, :name => "car"})
-cart.add_item RealItem.new({ :weight => 100, :price => 150, :name => "car"})
-cart.add_item RealItem.new({ :weight => 100, :price => 120, :name => "kettle"})
-
 order = Order.new
-@items.each { |i| order.add_item(i) }
 order.place
+puts order.placed_at.strftime("%b %-d, %Y %H:%M:%S") # Jan 1, 1970 15:00:00
