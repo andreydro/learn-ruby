e01ab3a0b055c8d9ec47f5ffc2b76ee220a9fa57
dro14 <and-rey-2309@mail.ru>
2016-06-06 19:12:54 +0300

    Finish 46 episode

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -7,12 +7,22 @@ require_relative "../app/cart"
 
 describe Cart do
 
-  it "adds items into the cart" do
-    cart = Cart.new("roman")
-    item1 = Item.new("kettle", price: 200)
-    item2 = Item.new("car", price: 200)
-    cart.add_items(item1, item2)
-    cart.items.should include(item1, item2)
+  describe "managing items" do
+
+    it "adds items into the cart" do
+      cart = Cart.new("roman")
+      item1 = Item.new("kettle", price: 200)
+      item2 = Item.new("car", price: 200)
+      cart.add_items(item1, item2)
+      cart.items.should include(item1, item2)
+    end
+
+    it "removes items from itself"
+
   end
+
+  it "counts items in itself"
+  it "placed order using all the items that were added inti the cart"
+  it "clears itself off the items after an order is placed"
 
 end
